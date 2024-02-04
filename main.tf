25a9423258935456c157cf7de221e21269e59231
saj <sajhad_altaf@hotmail.com>
2024-02-04 12:40:19 +0000

    remove test

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
-test
+provider "aws" {
+  region = "eu-west-2"  # Set your desired AWS region
+}
+resource "aws_instance" "example" {
+  ami           = "ami-0903ff18cc3c8e341"  # Set your desired AMI ID
+  instance_type = "t2.micro"  # Set your desired instance type
+  key_name = "sundaytestbitnamiwest2" # Replace with your EC2 key pair
+  tags = {
+    Name = "sundayexample_EUwest2_LDN-instance0402"
+  }
+}
