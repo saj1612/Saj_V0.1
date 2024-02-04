9fcb553c5195e1f9279532da6a2e541d9da187a0
saj <sajhad_altaf@hotmail.com>
2024-02-04 16:29:50 +0000

    backend s3

--- main.tf
+++ main.tf
@@ -4,11 +4,18 @@ provider "aws" {
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-up-and-running-state-saj"
 }
-
 resource "aws_instance" "test5" {
   ami           = "ami-0903ff18cc3c8e341" # Replace with the desired Amazon Linux 2 AMI for eu-west-2 (bitnami image)
   instance_type = "t2.micro"  # Change to the desired instance type
   tags = {
     Name = "sundayexample-instance"
 }
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-up-and-running-state-saj"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
 }
