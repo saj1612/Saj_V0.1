c35bfe2f6f57457e30dc5a5244310dcb36d285bc
saj <sajhad_altaf@hotmail.com>
2024-02-04 16:18:39 +0000

    removed backend part of code

--- main.tf
+++ main.tf
@@ -4,14 +4,7 @@ provider "aws" {
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-up-and-running-state-saj"
 }
-terraform {
-  backend "s3" {
-    # Replace this with your bucket name!
-    bucket         = "terraform-up-and-running-state-saj"
-    key            = "global/s3/terraform.tfstate"
-    region         = "eu-west-2"
-  }
-}
+
 resource "aws_instance" "test5" {
   ami           = "ami-0903ff18cc3c8e341" # Replace with the desired Amazon Linux 2 AMI for eu-west-2 (bitnami image)
   instance_type = "t2.micro"  # Change to the desired instance type
