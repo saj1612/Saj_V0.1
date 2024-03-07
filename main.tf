013b1158cd1f5fbd33b583811cca1a762d6dc8f8
saj <sajhad_altaf@hotmail.com>
2024-03-07 17:22:26 +0000

    sajtest s3 bucket added

--- main.tf
+++ main.tf
@@ -4,6 +4,9 @@ provider "aws" {
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-up-and-running-state-saj"
 }
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-up-and-running-state-sajTest"
+}
 resource "aws_instance" "test5" {
   ami           = "ami-0903ff18cc3c8e341" # Replace with the desired Amazon Linux 2 AMI for eu-west-2 (bitnami image)
   instance_type = "t2.micro"  # Change to the desired instance type
